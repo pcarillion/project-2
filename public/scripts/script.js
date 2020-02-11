81782732f8552aec87c9e476fbacc842ded5192b
Paul Carillion <pcarillion@yahoo.fr>
2020-02-11 11:42:49 +0100

    css

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,6 +6,7 @@ bikeContainer.forEach((val, i) => {
     i%2 ? val.className += " redBackground" : "";
 })
 
+
 function topFunction() {
     console.log("hey")
     document.body.scrollTop = 0; // For Safari
@@ -13,3 +14,32 @@ function topFunction() {
   }
 
   mybutton.onclick = topFunction
+
+
+
+  var $bikeCollection = $('#bike-collection').isotope({
+    itemSelector: '.bike-container',
+ });
+
+ console.log($bikeCollection)
+
+
+
+  $('.filter-button-group').on( 'click', 'button', function() {
+      console.log("button clicked")
+    var filterValue = $(this).attr('data-filter');
+        console.log(filterValue)
+    $bikeCollection.isotope({ filter: filterValue });
+  });
+
+  
+
+//   $bikeCollection.isotope({
+//     // filter element with numbers greater than 50
+//     filter: function() {
+//       // _this_ is the item element. Get text of element's .number
+//       var number = $(bike-container).find('.engine').text();
+//       // return true to show, false to hide
+//       return parseInt(engine, 10 ) > 50;
+//     }
+//   })
