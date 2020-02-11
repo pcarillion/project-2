fbd2fb5da893b93898a98759fbdeb9906cfb7411
Paul Carillion <pcarillion@yahoo.fr>
2020-02-11 16:46:22 +0100

    filters finished

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,25 +21,57 @@ function topFunction() {
     itemSelector: '.bike-container',
  });
 
+
  console.log($bikeCollection)
 
 
+ var filterValueCyl;
+ var filterValueBrand;
+ var filterValueType;
 
   $('#brandButtons').on( 'click', 'button', function() {
       console.log("button clicked")
-    var filterValue = $(this).attr('data-filter');
-        console.log(filterValue)
+    filterValueBrand = $(this).attr('data-filter');
+        console.log(filterValueBrand)
     // filterValue = filtrerCylindrée[ filterValue ] || filterValue;
-    $bikeCollection.isotope({ filter: filterValue });
+    // return filterValueBrand
+    $bikeCollection.isotope({ filter: filterValueBrand });
   });
 
   $('#engineButtons').on( 'click', 'button', function() {
-    console.log("button clicked")
-  var filterValue = $(this).attr('data-filter');
-      console.log(filterValue)
-  filterValue = filtrerCylindrée[ filterValue ] || filterValue;
-  $bikeCollection.isotope({ filter: filterValue });
+    // console.log("button clicked")
+  filterValueCyl = $(this).attr('data-filter');
+      console.log(filterValueCyl)
+  filterValueCyl = filtrerCylindrée[ filterValueCyl ] || filterValueCyl;
+//   return filterValueCyl
+  $bikeCollection.isotope({ filter: filterValueCyl });
 });
+
+
+$('#typeButtons').on( 'click', 'button', function() {
+    console.log("button clicked")
+  filterValueType = $(this).attr('data-filter');
+      console.log(filterValueType)
+  // filterValue = filtrerCylindrée[ filterValue ] || filterValue;
+  // return filterValueBrand
+  $bikeCollection.isotope({ filter: filterValueType });
+});
+
+//  $bikeCollection.isotope({ filter: filterValue })
+
+// //   $bikeCollection.forEach((val, i) => {
+// //     i%2 ? val.className += " redBackground" : "";
+// // })
+
+//     // var elements = document.querySelectorAll(".bike-container");
+//     // elements.forEach((val, i) => {
+//     //     var notDisplayed = getComputedStyle(val).getPropertyValue("display");
+//     //     console.log(notDisplayed)
+//     //     if (notDisplayed == "none") elements[i].remove();
+//     // })
+//     // console.log(elements)
+
+
 
 
   
@@ -51,6 +83,7 @@ function topFunction() {
       var engine = $(this).find('.engine').text();
       // return true to show, false to hide
       console.log(engine)
+      console.log(parseInt(engine, 10 ) < 500)
       return parseInt(engine, 10 ) < 500;
     },
 
@@ -69,12 +102,49 @@ function topFunction() {
     }
   }
 
-  $('.button-group').each( function( i, buttonGroup ) {
+
+  
+
+//   var filters = {};
+
+//   $('.filter-button-group').on('click', 'button', 
+
+
+//     function(event) {
+
+//         console.log("hey")
+
+//         var $button = $(event.currentTarget);
+//         console.log($button)
+
+//         var $buttonGroup = $button.parents('.buttonGroup');
+
+//         var filterGroup = $buttonGroup.attr('data-filter-group');
+
+//         filters[ filterGroup ] = $button.attr('datafilter');
+
+//         var filterValue = concatValues( filters );
+
+//         $bikeCollection.isotope({filter : filterValue})
+//     }
+//   )
+
+//   function concatValues( obj ) {
+//     var value = '';
+//     for ( var prop in obj ) {
+//       value += obj[ prop ];
+//     }
+//     return value;
+//   }
+  
+
+
+
+
+$('.button-group').each( function( i, buttonGroup ) {
     var $buttonGroup = $( buttonGroup );
     $buttonGroup.on( 'click', 'button', function() {
       $buttonGroup.find('.is-checked').removeClass('is-checked');
       $( this ).addClass('is-checked');
     });
   });
-
-  
